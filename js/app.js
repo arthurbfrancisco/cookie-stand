4fc328e63cd298a021553786e87647a4a6d10246
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-12 09:04:32 +0700

    update

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ function renderHours(){
   }
   //only makes total head to right
   let total = document.createElement('td');
-  total.textContent = "total"
+  total.textContent = "Daily Locatation Total"
   tableElement.appendChild(total)
 }
 
@@ -88,6 +88,7 @@ Tokyo.tableRender();
 Dubai.tableRender();
 Paris.tableRender();
 Lima.tableRender();
+
 // // Define an array of strings representing the hours of operation for the Seattle store
 // let hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', 'Total']
 
