a08213684a4fdd4f771ce3380adf3a7db2cf9121
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-04-12 10:33:37 +0700

    update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 //Array for hours of ....
 let hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm']
 
-const tableElement = document.getElementById("Table");
+const tableElement = document.getElementById("storeProfiles");
 
 function Store(name, min, max, avg) {
     this.name = name,
